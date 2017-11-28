2c9baf4a440ad39519f29a8a35fb1d36f42af90d
Marta <martadabrowka@gmail.com>
2017-11-28 16:25:50 +0000

    passed 4 tests

--- src/org/list/LinkedList.java
+++ src/org/list/LinkedList.java
@@ -1,4 +1,41 @@
 package org.list;
 
+class Node {
+    public int value;
+    public Node next;
+
+}
+
+
 public class LinkedList {
+
+    private Node head = null;
+
+//   LinkedList list;
+//
+//    public LinkedList() {
+//        this.list = new LinkedList();
+//    }
+
+//    LinkedList list = new LinkedList();
+
+    private int count = 0;
+    private int first = 0;
+
+
+    public int size() {
+        return count;
+    }
+
+    public int insert(int value) {
+        Node newNode = new Node();
+        newNode.value = value;
+        newNode.next = head;
+        head = newNode;
+        return count++;
+    }
+
+    public int head() {
+        return head.value;
+    }
 }
--- test/org/list/LinkedListTest.java
+++ test/org/list/LinkedListTest.java
@@ -1,13 +1,52 @@
 package org.list;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class LinkedListTest {
+    LinkedList list;
+
+    @Before
+    public void before() {
+        list = new LinkedList();
+    }
 
     @Test
     public void WhenCreated_LinkedListHasNoElements() {
+        assertEquals(0, list.size());
 
     }
+
+    @Test
+    public void WhenSingleItemInsertedHasSizeOne() {
+        list.insert(33);
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void WhenItemInsertedHeadEqualsItem() {
+        list.insert(42);
+        assertEquals(42, list.head());
+
+    }
+
+    @Test
+    public void WhenAnotherItemInsertedItIsHead() {
+        list.insert(42);
+        list.insert(43);
+        assertEquals(43, list.head());
+    }
+
+    @Test
+    public void WhenTwoItemsInsertedSizeIsTwo() {
+        list.insert(42);
+        list.insert(43);
+        assertEquals(2, list.size());
+    }
+
+
 
 
 }
